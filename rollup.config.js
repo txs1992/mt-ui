7eb9edb5edd7f20bd491cfe8d72a94ac69ff23d9
TaoXuSheng <xusheng.tao@ele.me>
2018-06-12 18:24:40 +0800

    add eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,27 @@
 import babel from 'rollup-plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+import eslint from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  input: './src/index.js',
+  input: 'src/index.js',
   output: {
-    file: './lib/mt-ui.js',
-    format: 'cjs'
+    file: 'lib/mu-ui.js',
+    name: 'MTUI',
+    format: 'umd'
   },
   plugins: [
+    eslint({
+      throwError: true,
+      exclude: 'node_modules/**'
+    }),
+    resolve({
+      extensions: ['.js']
+    }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers']
-    })
+    }),
+    uglify()
   ]
 }
